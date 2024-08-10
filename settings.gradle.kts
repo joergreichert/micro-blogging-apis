d5df901ab97844e4f565e3d51b15eedbf410dcc6
Jörg Reichert <joergreichert@users.noreply.github.com>
2024-08-10 13:51:54 +0200

    update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.buildFileName = "build.gradle.kts"
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.3"
+    id("de.fayard.refreshVersions") version "0.60.5"
 }
