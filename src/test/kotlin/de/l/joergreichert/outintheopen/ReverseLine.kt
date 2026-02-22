9423270390f53cbb08f4a2d0f09d1401f9182a9d
Jörg Reichert <joergreichert@users.noreply.github.com>
2026-02-22 12:11:17 +0100

    revert to 23

--- src/test/kotlin/de/l/joergreichert/outintheopen/ReverseLine.kt
+++ src/test/kotlin/de/l/joergreichert/outintheopen/ReverseLine.kt
@@ -1,5 +1,6 @@
 package de.l.joergreichert.outintheopen
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.FileWriter
 import java.nio.file.Paths
@@ -8,6 +9,7 @@ import kotlin.io.path.readLines
 class ReverseLine {
 
     @Test
+    @Disabled
     fun test() {
         val lines = Paths.get("file:///Users/joerg/Desktop/out-in-the-open/masto-2026-02-21-1200-books.txt").readLines().reversed()
         FileWriter("file:///Users/joerg/Desktop/out-in-the-open/masto-2026-02-21-1200-books-rev.txt").write(lines.joinToString("\n"))
